dc54b46d7dce537fde912d4dbe4e126f74bf84ce
djay.cse <djay.cse@gmail.com>
2025-10-22 20:50:41 +1100

    General fixes

--- sh/start_opentdm.sh
+++ sh/start_opentdm.sh
@@ -1,1 +1,1 @@
-screen -A -m -d -S opentdm ./q2proded-x86_64_3822-cse001 +set net_port 27920 +set game opentdm +set dedicated 1 +exec opentdm.cfg +map q2dm1
+screen -A -m -d -S opentdm ./q2proded-x86_64_3822-cse001 +set net_port 27920 +set game opentdm +set dedicated 1 +exec q2pro-opentdm.cfg +exec home.cfg +map q2dm1
--- sh/start_tourney-insta.sh
+++ sh/start_tourney-insta.sh
@@ -1,1 +1,1 @@
-screen -A -m -d -S tourney-insta ./r1q2ded-i386_7864-debug +set port 27930 +set game tourney-insta +set dedicated 1 +exec tourney-insta.cfg +map q2rdm9
+screen -A -m -d -S tourney-insta ./r1q2ded-i386_7864-debug +set port 27930 +set game tourney-insta +set dedicated 1 +exec q2pro-tourney-insta.cfg +exec home.cfg +map q2rdm2
