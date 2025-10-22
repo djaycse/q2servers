8a149a155a0ba073e051eccefe4179f5e160f3d9
djay.cse <djay.cse@gmail.com>
2025-10-23 00:48:47 +1100

    Fixed back and delete script failing

--- sh/backup_del_logs_restart_all.sh
+++ sh/backup_del_logs_restart_all.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-set +e  # continue on error
-cd "$(dirname "$0")"
 
-echo "Killing processes..."
 ./sh/kill_all.sh
-
-echo "Backing up..."
 ./sh/backup_all.sh
-
-echo "Deleting logs..."
 ./sh/del_logs_all.sh
-
-echo "Starting servers..."
 ./sh/start_all.sh
 
-echo "All scripts executed."
+screen -list
