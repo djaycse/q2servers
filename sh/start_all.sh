70112a61637985f6f0adb58fb17f3e5769a17a03
djay.cse <djay.cse@gmail.com>
2025-10-23 00:19:18 +1100

    Tweaks to start_all.sh

--- sh/start_all.sh
+++ sh/start_all.sh
@@ -1,5 +1,18 @@
-./sh/start_openffa.sh
-./sh/start_opentdm.sh
-./sh/start_tourney-insta.sh
-./sh/start_tourney-ffa.sh
+#!/bin/bash
+set +e  # continue on error
+cd "$(dirname "$0")"
+
+echo "Starting OpenFFA..."
+./start_openffa.sh
+
+echo "Starting OpenTDM..."
+./start_opentdm.sh
+
+echo "Starting Tourney-insta..."
+./start_tourney-insta.sh
+
+echo "Starting Tourney-FFA..."
+./start_tourney-ffa.sh
+
+echo "All scripts executed."
 screen -list
