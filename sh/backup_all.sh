308b0b29b374b3cbbc58171494a5b343ace117e0
djay.cse <djay.cse@gmail.com>
2025-10-22 23:59:44 +1100

    Fixed OpenFFA vote mask to enable correct vote options. Backup script now targets specific files and folders.

--- sh/backup_all.sh
+++ sh/backup_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-zip -r backups/openffa-`date +%Y-%m-%d`.zip openffa/*
-zip -r backups/opentdm-`date +%Y-%m-%d`.zip opentdm/*
-zip -r backups/tourney-insta-`date +%Y-%m-%d`.zip tourney-insta/*
-zip -r backups/tourney-ffa-`date +%Y-%m-%d`.zip tourney-ffa/*
+zip -r backups/openffa-`date +%Y-%m-%d`.zip openffa/logs openffa/highscores openffa/save openffa/q2admin.log
+zip -r backups/opentdm-`date +%Y-%m-%d`.zip opentdm/logs opentdm/highscores opentdm/save opentdm/q2admin.log
+zip -r backups/tourney-insta-`date +%Y-%m-%d`.zip tourney-insta/qconsole.log tourney-insta/highscores tourney-insta/save tourney-insta/q2admin.log
+zip -r backups/tourney-ffa-`date +%Y-%m-%d`.zip tourney-ffa/qconsole.log tourney-ffa/highscores tourney-ffa/save tourney-ffa/q2admin.log
