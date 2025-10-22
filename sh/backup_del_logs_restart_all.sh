ca18efd25a7c134f3aeeeb37613eeb4a8fd83d93
djay.cse <djay.cse@gmail.com>
2025-10-23 00:11:45 +1100

    Tweaks to backup_del_logs_restart_all.sh

--- sh/backup_del_logs_restart_all.sh
+++ sh/backup_del_logs_restart_all.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+set +e  # continue on error
+cd "$(dirname "$0")"
+
+echo "Killing processes..."
 ./sh/kill_all.sh
+
+echo "Backing up..."
 ./sh/backup_all.sh
-./sh/dell_logs_all.sh
+
+echo "Deleting logs..."
+./sh/del_logs_all.sh
+
+echo "Starting servers..."
 ./sh/start_all.sh
+
+echo "All scripts executed."
