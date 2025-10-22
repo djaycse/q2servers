cb3c1eed97b5dea731c9c4d61fa2d4d9ecac6a2a
djay.cse <djay.cse@gmail.com>
2025-10-22 23:39:59 +1100

    Fixed commands for backup_all.sh

--- sh/backup_all.sh
+++ sh/backup_all.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-cd /home/quake2/aq2/quake2/action/ && tar -zcvf /home/quake2/backups/aq2-backup-`date +%Y-%m-%d`.tar.gz *.* &&
-cd /home/quake2/ctf/quake2/ctf/ && tar -zcvf /home/quake2/backups/ctf-backup-`date +%Y-%m-%d`.tar.gz *.* &&
-cd /home/quake2/gloom/quake2/gloom/ && tar -zcvf /home/quake2/backups/gloom-backup-`date +%Y-%m-%d`.tar.gz *.* logs/*.* &&
-cd /home/quake2/opentdm/quake2/opentdm/ && tar -zcvf /home/quake2/backups/opentdm-backup-`date +%Y-%m-%d`.tar.gz *.* &&
-cd /home/quake2/opentdm2/quake2/opentdm/ && tar -zcvf /home/quake2/backups/opentdm2-backup-`date +%Y-%m-%d`.tar.gz *.* &&
-cd /home/quake2/osp/quake2/tourney/ && tar -zcvf /home/quake2/backups/osp-backup-`date +%Y-%m-%d`.tar.gz *.* &&
-cd /home/quake2/quake2/quake2/tourney/ && tar -zcvf /home/quake2/backups/quake2-backup-`date +%Y-%m-%d`.tar.gz *.* &&
-cd /home/quake2/ra2/quake2/arena/ && tar -zcvf /home/quake2/backups/ra2-backup-`date +%Y-%m-%d`.tar.gz *.*
+zip -r backups/openffa-`date +%Y-%m-%d`.zip openffa/*
+zip -r backups/opentdm-`date +%Y-%m-%d`.zip opentdm/*
+zip -r backups/tourney-insta-`date +%Y-%m-%d`.zip tourney-insta/*
+zip -r backups/tourney-ffa-`date +%Y-%m-%d`.zip tourney-ffa/*
